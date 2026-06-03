e4bdb3821092d109d2a1f3eba890b1b130455101
Ismail Basyir Muthahhar <Ismailbm31@gmail.com>
2026-06-03 21:23:14 +0700

    Refactor : Loan.java

--- src/main/java/com/p2plending/domain/loan/Loan.java
+++ src/main/java/com/p2plending/domain/loan/Loan.java
@@ -15,7 +15,7 @@ public class Loan {
     private final LocalDateTime createdAt;
 
     private LoanStatus status;
-    private transient LoanState stateBehavior;
+    private LoanState stateBehavior;
 
     public Loan(String id, Borrower borrower, BigDecimal amount, int tenor, String purpose) {
         // Validasi Domain: Mencegah pembuatan objek jika limit tidak cukup
